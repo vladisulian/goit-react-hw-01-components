cd55a8be873beb5ead9d76b3bc2fba8b69736bbf
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-30 01:05:52 +0200

    The 1 task style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
-import { Profile } from './Profile';
+import { Profile } from './Profile/Profile';
 import user from './user.json';
 
 export const App = () => {
   return <Profile user={user} />;
 };
-console.log('In App', user);
