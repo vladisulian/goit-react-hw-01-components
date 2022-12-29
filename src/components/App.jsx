bd6feec6e5cc60c433a6c7171e5c9e3228afe612
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-29 23:17:05 +0200

    The first task is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import { Profile } from './Profile';
+import user from './user.json';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Profile user={user} />;
 };
+console.log('In App', user);
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+export const Profile = ({ user }) => {
+  return (
+    <div>
+      <div className="profile">
+        <div className="description">
+          <img src={user.avatar} alt="User avatar" className="avatar" />
+          <p className="name">{user.username}</p>
+          <p className="tag">@{user.tag}</p>
+          <p className="location">{user.location}</p>
+        </div>
+
+        <ul className="stats">
+          <li>
+            <span className="label">Followers</span>
+            <span className="quantity"> {user.stats.followers}</span>
+          </li>
+          <li>
+            <span className="label">Views</span>
+            <span className="quantity"> {user.stats.views}</span>
+          </li>
+          <li>
+            <span className="label">Likes</span>
+            <span className="quantity"> {user.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
