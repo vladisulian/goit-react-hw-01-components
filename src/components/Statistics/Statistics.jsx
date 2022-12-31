eed8dc749906b35caad1b288ca7ca0d84f2756a4
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-31 15:43:15 +0200

    Add if rule

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 export const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
-      <h2 className="title">{title}</h2>
+      {title && <h2 className="title">{title}</h2>}
       <ul className="stat-list">
         {stats.map(stat => (
           <li key={stat.id}>
