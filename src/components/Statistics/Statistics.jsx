04dcb30c5163859eb2d4fa0dfef0ddcf8ded9099
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-31 15:21:35 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,8 +5,8 @@ export const Statistics = ({ title, stats }) => {
       <ul className="stat-list">
         {stats.map(stat => (
           <li key={stat.id}>
-            {stat.label} <br />
-            {stat.percentage}
+            <span>{stat.label}</span>
+            <span>{stat.percentage}</span>
           </li>
         ))}
       </ul>
