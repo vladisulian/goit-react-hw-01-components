65b4276674ccdfa3940c4cf46fadc7422906f504
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-31 20:47:31 +0200

    Make flex-render (2 projects)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,18 @@ import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
-    // <Profile
-    //   username={user.username}
-    //   tag={user.tag}
-    //   location={user.location}
-    //   avatar={user.avatar}
-    //   followers={user.stats.followers}
-    //   views={user.stats.views}
-    //   likes={user.stats.likes}
-    // ></Profile>
-    <Statistics title="Upload stats" stats={data}></Statistics>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    >
+      <Statistics title="Upload stats" stats={data} />
+    </Profile>
+
     // (<Statistics stats={data}></Statistics>)
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ export const Statistics = ({ title, stats }) => {
       {title && <h2 className="title">{title}</h2>}
       <ul className="stat-list">
         {stats.map(stat => (
-          <li key={stat.id}>
+          <li key={stat.id} className="list-item">
             <span className="label">{stat.label}</span> <br />
             <span className="percentage">{stat.percentage}%</span>
           </li>
@@ -29,7 +29,7 @@ setInterval(() => {
 }, interval);
 
 function switchColor() {
-  const listItem = document.querySelectorAll('li');
+  const listItem = document.querySelectorAll('.list-item');
 
   listItem.forEach(e => {
     e.style.background = `#${Math.floor(Math.random() * 16777215)
